b7c6caef5877faa736cf0f85e9d55bcc15378786
sunilpaulmathew <sunil.kde@gmail.com>
2021-12-31 22:46:23 +0100

    Added details view
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- demo/src/main/java/in/sunilpaulmathew/covidstats/demo/RecyclerViewAdapter.java
+++ demo/src/main/java/in/sunilpaulmathew/covidstats/demo/RecyclerViewAdapter.java
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Color;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +15,12 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textview.MaterialTextView;
 
 import java.util.List;
+
+import in.sunilpaulmathew.covidstats.CovidStats;
 
 /*
  * Created by sunilpaulmathew <sunil.kde@gmail.com> on August 25, 2021
@@ -72,8 +78,46 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             this.mNewD = view.findViewById(R.id.new_deaths);
         }
 
+        @SuppressLint("SetTextI18n")
         @Override
         public void onClick(View view) {
+            LayoutInflater mInflater = LayoutInflater.from(view.getContext());
+            View aboutLayout = mInflater.inflate(R.layout.layout_details, null);
+            MaterialTextView mTDeaths = aboutLayout.findViewById(R.id.total_deaths);
+            MaterialTextView mNDeaths = aboutLayout.findViewById(R.id.new_deaths);
+            MaterialTextView mTCases = aboutLayout.findViewById(R.id.total_cases);
+            MaterialTextView mNCases = aboutLayout.findViewById(R.id.new_cases);
+            MaterialTextView mCountry = aboutLayout.findViewById(R.id.country);
+
+            Spannable totalDeaths = new SpannableString("" + data.get(getAdapterPosition()).getTotalDeaths());
+            totalDeaths.setSpan(new ForegroundColorSpan(Color.RED), 0, totalDeaths.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            mTDeaths.setText(view.getContext().getString(R.string.deaths_total_details, CovidStats.getLastUpdatedDate(
+                    CovidStats.getCountries().get(getAdapterPosition()))) + " ");
+            mTDeaths.append(totalDeaths);
+
+            Spannable newDeaths = new SpannableString("" + data.get(getAdapterPosition()).getNewDeaths());
+            newDeaths.setSpan(new ForegroundColorSpan(Color.RED), 0, newDeaths.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            mNDeaths.setText(view.getContext().getString(R.string.deaths_new_details, CovidStats.getLastUpdatedDate(
+                    CovidStats.getCountries().get(getAdapterPosition()))) + " ");
+            mNDeaths.append(newDeaths);
+
+            Spannable totalCases = new SpannableString("" + data.get(getAdapterPosition()).getTotalCases());
+            totalCases.setSpan(new ForegroundColorSpan(Color.GREEN), 0, totalCases.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            mTCases.setText(view.getContext().getString(R.string.cases_total_details, CovidStats.getLastUpdatedDate(
+                    CovidStats.getCountries().get(getAdapterPosition()))) + " ");
+            mTCases.append(totalCases);
+
+            Spannable newCases = new SpannableString("" + data.get(getAdapterPosition()).getNewCases());
+            newCases.setSpan(new ForegroundColorSpan(Color.GREEN), 0, newCases.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            mNCases.setText(view.getContext().getString(R.string.cases_new_details, CovidStats.getLastUpdatedDate(
+                    CovidStats.getCountries().get(getAdapterPosition()))) + " ");
+            mNCases.append(newCases);
+            mCountry.setText(data.get(getAdapterPosition()).getCountry().toUpperCase());
+
+            new MaterialAlertDialogBuilder(view.getContext())
+                    .setView(aboutLayout)
+                    .setPositiveButton(R.string.dismiss, (dialogInterface, i) -> {
+                    }).show();
         }
     }
 
